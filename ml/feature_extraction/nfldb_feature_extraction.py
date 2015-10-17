1a2f3a08dae47ca41f44149ef2ff2f2ef8063179
Ross Boberg <ross.boberg@gmail.com>
2015-10-16 22:21:58 -0700

    add nfldb helpers, add predictions for all next week players, separate predictions of played vs stats assuming played, pickle pipelines and data cache, improve data collection for rookies"

--- ml/feature_extraction/nfldb_feature_extraction.py
+++ ml/feature_extraction/nfldb_feature_extraction.py
@@ -6,7 +6,6 @@ import math
 
 from sklearn.base import TransformerMixin
 
-
 # rs_time stands for regular season time
 # this is elapsed weeks of regular season
 # since the 1970 merger. It's currently
@@ -43,12 +42,12 @@ def fill_time_of_group(group, stats, player_info, group_by='player_id'):
 	# add rows for missing times
 	group = group.reindex(all_t)
 	# wherever player_id is missing, create feature called played=0, else played=1
-	group['played'] = pd.isnull(group['player_id']) == False
+	#group['played'] = pd.isnull(group['player_id']) == False
+	group['played'] = group['played'].fillna(False)
 	# pad player_info
-	player_info = ['player_id','full_name','position']
+	#player_info = ['player_id','full_name','position']
 	group[player_info] = group[player_info].fillna(method='pad')
 	# 0 for all stats
-	stats = ['rushing_yds','rushing_att']
 	group[stats] = group[stats].fillna(0)
 	# add year week for all
 	yr_wk_df = pd.DataFrame(list(pd.Series(all_t).apply(inverse_rs_time)), index=all_t)
@@ -57,10 +56,10 @@ def fill_time_of_group(group, stats, player_info, group_by='player_id'):
 	# return
 	return group
 
-
+# GOAL: get all yr_wk asked for all player_ids, 0ing any stats for weeks that were skipped
 
 # takes a nfldb database link (db) and returns a dataframe
-# where each row is a plyear in a year in a week
+# where each row is a player in a year in a week
 # yr_wk is a list of (year, week) tuples
 # stats is a list of stats to pull from each PlayPlayer
 # player_info is a list of information to pull for each player
@@ -68,8 +67,12 @@ def fill_time_of_group(group, stats, player_info, group_by='player_id'):
 # returns the data frame indexed by rs_time (regular season time)
 # fill_time indicates whether to fill missing time in the player's career with 0 stats
 # if fill_time == True it also creates a binary feature called 'played'
-def player_week2dataframe(db, yr_wk, stats, player_info, position='RB', fill_time=True):
+# if player_ids is specified, it WILL return stats for those players for EVERY yr_wk asked for
+# even if a player wasn't an active NFL player at the time - the stats will just be 0.
+def player_week2dataframe(db, yr_wk, stats, player_info, player_ids=None, position='RB', fill_time=True):
 	player_list = []
+	player_objects = {player.player_id:player for player in nfldb.Query(db).player(player_id=player_ids).as_players()}
+
 	for yr, wk in yr_wk:
 		# query for this weak
 		q = nfldb.Query(db)
@@ -77,15 +80,33 @@ def player_week2dataframe(db, yr_wk, stats, player_info, position='RB', fill_tim
 		q.game(season_year=yr, week=wk, season_type='Regular')
 		# maybe filter for position
 		if(position):
-			q.player(position=position)	
+			q.player(position=position)
+		if(player_ids):
+			q.player(player_id=player_ids)
+			player_ids_i = list(player_ids)
 		# loop through PlayPlayer objects, aggregated for the week
 		for pp in q.as_aggregate():
+			player = pp.player
 			pobj = {'year':yr, 'week':wk}
+			pid = player.player_id
 			for field in player_info:
-				pobj[field] = getattr(pp.player,field)
+				pobj[field] = getattr(player,field)
 			for field in stats:
 				pobj[field] = getattr(pp,field)
+			pobj['played'] = True
 			player_list += [pobj]
+			if(player_ids):
+				player_ids_i.remove(pid)
+		if(player_ids):
+			for pid in player_ids_i:
+				player = player_objects[pid]
+				pobj = {'year':yr, 'week':wk}
+				for field in player_info:
+					pobj[field] = getattr(player,field)
+				for field in stats:
+					pobj[field] = np.nan
+				pobj['played'] = False
+				player_list += [pobj]
 	pdf = pd.DataFrame(player_list)
 	pdf['time'] = pdf.apply(rs_time_df, 1)
 	pdf.set_index('time', inplace=True)
@@ -142,16 +163,37 @@ def fill_nan(df, value=0):
 	return(df.fillna(value=value))
 
 ### Make variable that captures average of group
-def make_mean_data_group(df, mean_cols):
+def make_mean_data_group(df, mean_cols, start_at_first_played=True):
 	def col_str(col):
 		return col + '_mean';
-	dfout = pd.rolling_mean(df[mean_cols].fillna(value=0), min_periods=0, window=10000000)
+	# start the mean at the first played game
+	if start_at_first_played and ('played' in df.columns):
+		# find the index of the first played game
+		# player may have never played:
+		if df['played'].any():
+			first_played = df['played'].ravel().nonzero()[0][0]
+		else:
+			first_played = -1
+	else:
+		first_played = df.index[0]
+
+
+	dfout = df[mean_cols].copy()
+	if first_played == -1:
+		# special case that the player has never played before
+		dfout[:] = 0
+	else:
+		# get the mean
+		dfout.iloc[first_played:] = pd.rolling_mean(dfout.iloc[first_played:].fillna(value=0), min_periods=0, window=10000000)
+		# set the mean to zero before that game
+		dfout.iloc[:first_played] = 0
+
 	dfout.columns = [col_str(col) for col in dfout.columns]
 	dfout = pd.concat([df, dfout], axis=1)
 	return dfout
 
 ### Make variable that captures average of group
-def make_mean_data(df, mean_cols, groupby_cols = ['player_id']):
+def make_mean_data(df, mean_cols, groupby_cols = ['player_id'], start_at_first_played=True):
 	grouped = df.groupby(groupby_cols)
 	df_out=None
 	for name, group in grouped:
@@ -166,10 +208,14 @@ def make_mean_data(df, mean_cols, groupby_cols = ['player_id']):
 # if they didnt play in more than pct_played_threshold*n of the n lagged games
 # the row gets thrown out
 def filter_played_percent(df, pct_played_threshold):
+	if(pct_played_threshold <= 0):
+		return df
+	if(pct_played_threshold > 1):
+		raise ValueError('pct_played_threshold of more than 1 will throw out all observations')
 	re_pat = re.compile('played_lag')
 	col_names = df.columns.values.tolist()
 	col_matches = [col for col in col_names if re_pat.search(col)]
-	return df[df[col_matches].fillna(value=0).mean(axis=1) > pct_played_threshold]
+	return df[df[col_matches].fillna(value=0).mean(axis=1) >= pct_played_threshold]
 
 
 
@@ -192,9 +238,9 @@ class WeeklyPlayerData(TransformerMixin):
 		# this function essentially pulls data
 		# However, may change that later - X may 
 		# be a list of player names to get or something
-		return player_week2dataframe(db=self.db, yr_wk=self.yr_wk, stats=self.stats, fill_time=self.fill_time, player_info=self.player_info, position=self.position)
+		return player_week2dataframe(player_ids=X, db=self.db, yr_wk=self.yr_wk, stats=self.stats, fill_time=self.fill_time, player_info=self.player_info, position=self.position)
 	def get_params(self, deep=True):
-		return {}
+		return {'stats':self.stats, 'player_info':self.player_info}
 	def set_params(self, **parameters):
 		for parameter, value in parameters.items():
 			setattr(self, parameter, value)
@@ -213,7 +259,7 @@ class LagPlayerData(TransformerMixin):
 	def transform(self, X):
 		return make_lag_data(X, nlag=self.nlag, groupby_cols=self.groupby_cols, lag_cols=self.lag_cols, same_year_bool=self.same_year_bool)
 	def get_params(self, deep=True):
-		return {}
+		return {'lag_cols':self.lag_cols}
 	def set_params(self, **parameters):
 		for parameter, value in parameters.items():
 			setattr(self, parameter, value)
@@ -228,7 +274,7 @@ class MeanPlayerData(TransformerMixin):
 	def transform(self, X):
 		return make_mean_data(X, groupby_cols=self.groupby_cols, mean_cols=self.mean_cols)
 	def get_params(self, deep=True):
-		return {}
+		return {'mean_cols':self.mean_cols}
 	def set_params(self, **parameters):
 		for parameter, value in parameters.items():
 			setattr(self, parameter, value)
--- ml/prediction/rb_stats.py
+++ ml/prediction/rb_stats.py
@@ -1,6 +1,7 @@
 import nfldb
 import pandas as pd
 import numpy as np
+import pickle
 
 from ml.feature_extraction.nfldb_feature_extraction import WeeklyPlayerData
 from ml.feature_extraction.nfldb_feature_extraction import LagPlayerData
@@ -8,6 +9,7 @@ from ml.feature_extraction.nfldb_feature_extraction import MeanPlayerData
 from ml.feature_extraction.nfldb_feature_extraction import ExtractColumns
 from ml.feature_extraction.nfldb_feature_extraction import HandleNaN
 from ml.feature_extraction.nfldb_feature_extraction import FilterPlayedPercent
+from ml.nfldb_helpers.generic_helpers import week_player_id_list
 
 from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression
@@ -16,7 +18,9 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error
 from sklearn.pipeline import Pipeline
 from sklearn.pipeline import FeatureUnion
 
-def train_test_split_index(n,test_size=0.2):
+def train_test_split_index(n,test_size=0.2,seed=None):
+	if(seed):
+		np.random.seed(seed)
 	rand_i = np.random.choice(range(n), n, replace=False)
 	test_i = rand_i[range(int(round(n*test_size)))]
 	train_i = rand_i[range(int(round(n*test_size)),n)]
@@ -29,7 +33,7 @@ def split_by_year_week(X, test_yr_wk):
 		match = False
 		row_yr_wk = (X.iloc[i]['year'], X.iloc[i]['week'])
 		for yr_wk in test_yr_wk:
-			if row_yr_wk == yr_wk:
+			if row_yr_wk[0] == yr_wk[0] and row_yr_wk[1] == yr_wk[1]:
 				match = True
 				test_i += [i]
 				break
@@ -42,39 +46,69 @@ def main():
 	# connect to nfldb
 	db = nfldb.connect()
 	result_path = '../results'
+	cache_path = '../data'
+	position = 'RB'
+	load_cached = True
 
-	# make player data transformer
-	yr_wk = [(j, i) for j in range(2012,2015) for i in range(1,18)]
-	yr_wk += [(2015, i) for i in range(1,5)]
+	if(not load_cached):
+		# make player data transformer
+		yr_wk = [(j, i) for j in range(2012,2015) for i in range(1,18)]
+		yr_wk += [(2015, i) for i in range(1,6)]
 
-	#stats = ['rushing_yds','rushing_att']
-	stats = ['receiving_rec', 'receiving_tar', 'receiving_tds', 'receiving_yac_yds', 'receiving_yds', 'rushing_att', 'rushing_tds','rushing_yds']
+		#stats = ['rushing_yds','rushing_att']
+		stats = ['receiving_rec', 'receiving_tar', 'receiving_tds', 'receiving_yac_yds', 'receiving_yds', 'rushing_att', 'rushing_tds','rushing_yds']
 
-	player_info = ['player_id','full_name','position']
-	playerData = WeeklyPlayerData(db=db, yr_wk=yr_wk, stats=stats, player_info=player_info, fill_time=True, position='RB')
+		player_info = ['player_id','full_name','position']
+		playerData = WeeklyPlayerData(db=db, yr_wk=yr_wk, stats=stats, player_info=player_info, fill_time=True, position=position)
 
-	# creates lags of the data
-	lag_cols = ['year', 'week', 'played'] + stats
-	lagData = LagPlayerData(nlag=6, groupby_cols=['player_id'], lag_cols=lag_cols, same_year_bool=True)
+		# creates lags of the data
+		lag_cols = ['year', 'week', 'played'] + stats
+		nlag = 6
+		lagData = LagPlayerData(nlag=nlag, groupby_cols=['player_id'], lag_cols=lag_cols, same_year_bool=True)
 
-	# creates means of the data
-	mean_cols = stats
-	meanData = MeanPlayerData(groupby_cols=['player_id'], mean_cols=mean_cols)
+		# creates means of the data
+		mean_cols = stats
+		meanData = MeanPlayerData(groupby_cols=['player_id'], mean_cols=mean_cols)
 
+		# pipeline for getting data
+		pipe1 = Pipeline(steps=[('data',playerData), ('lag',lagData), ('mean',meanData)])
+		#processed_data = pipe1.fit_transform(X=None)
 
-	# pipeline for getting data
-	#pipe = Pipeline(steps=[('data',playerData), ('lag',lagData)])
-	pipe1 = Pipeline(steps=[('data',playerData), ('lag',lagData), ('mean',meanData)])
-	processed_data = pipe1.fit_transform(X=None)
-	# print processed_data
+		# print processed_data
+		# pipeline for seting which columns we want and handling NaN
+		pct_played_threshold = 0.0
+		pipe2_steps = [('handle',HandleNaN(method='fill')), ('filterplayed',FilterPlayedPercent(pct_played_threshold=pct_played_threshold))]
+		pipe2 = Pipeline(steps=pipe2_steps)
 
+		pipe = Pipeline([('pipe1',pipe1),('pipe2',pipe2)])
 
-	# pipeline for seting which columns we want and handling NaN
-	pct_played_threshold = 0.6
-	pipe2_steps = [('handle',HandleNaN(method='fill')), ('filterplayed',FilterPlayedPercent(pct_played_threshold=pct_played_threshold))]
-	pipe2 = Pipeline(steps=pipe2_steps)
+		# pipe_params = {'pipe1__data__stats':stats,
+		# 'pipe1__data__player_info':player_info,
+		# 'pipe1__lag__lag_cols':lag_cols,
+		# 'pipe1__mean__mean_cols':mean_cols,
+		# 'pipe2__filterplayed__pct_played_threshold':pct_played_threshold
+		# }
 
-	all_columns = pipe2.fit_transform(X=processed_data)
+		all_columns = pipe.fit_transform(X=None)
+		all_columns.position = all_columns.position.astype(str)
+
+		pickle.dump(pipe.set_params(pipe1__data__db=None), open(cache_path + '/pipe_'+position+'.p', 'wb'))
+		#pickle.dump(pipe_params, open(cache_path + '/pipe_params_'+position+'.p', 'wb'))
+		pickle.dump(all_columns, open(cache_path + '/data_'+position+'.p', 'wb'))
+	else:
+		#pipe_params = pickle.load(open(cache_path + '/pipe_params_'+position+'.p', 'rb'))
+		all_columns = pickle.load(open(cache_path + '/data_'+position+'.p', 'rb'))
+		pipe = pickle.load(open(cache_path + '/pipe_'+position+'.p', 'rb'))
+		#pipe = pipe.set_params(**pipe_params)
+		pipe_params = pipe.get_params()
+		stats = pipe_params['pipe1__data__stats']
+		player_info = pipe_params['pipe1__data__player_info']
+		lag_cols = pipe_params['pipe1__lag__lag_cols']
+		mean_cols = pipe_params['pipe1__mean__mean_cols']
+
+	pipe.set_params(pipe1__data__db=db)
+
+	full_train = all_columns
 
 	# picks columns to model
 	lag_cols = [stat + '_lag' for stat in stats]
@@ -82,26 +116,53 @@ def main():
 	other_cols = ['same_year_lag', 'played_lag']
 
 	infoColumns = ExtractColumns(like=[], exact=['year','week','time','player_id','full_name'])
-	row_info = infoColumns.fit_transform(X=all_columns)
+	row_info = infoColumns.fit_transform(X=full_train)
 
-	yr_wk_pred = [(2015, 4)]
-	train_i, predict_i = split_by_year_week(all_columns, yr_wk_pred)
-	df_pred = row_info.iloc[predict_i]
+	### prediction data
+	# prediction pipeline
+	pred_data_pipe = Pipeline(steps=[('pipe1',pipe1),('pipe2',pipe2)])
+
+	# get information we need to make predictions
+	season_phase, cur_year, cur_week = nfldb.current(db)
+	pred_week = cur_week
+	pred_yr_wk = [(j, i) for j in range(2009,cur_year-1) for i in range(1,18)]
+	pred_yr_wk += [(cur_year, i) for i in range(1,pred_week+1)]
+
+	pred_data_pipe.set_params(pipe1__data__yr_wk = pred_yr_wk)
+
+	player_ids = week_player_id_list(db, cur_year, pred_week, position='RB')
+	#player_ids = player_ids[0:2] + player_ids[-3:-1]
+
+	pred_data = pred_data_pipe.fit_transform(player_ids)
+	pred_info = infoColumns.fit_transform(X=pred_data)
+
+	# predict for the last week
+	pred_yr_wk_t = [pred_yr_wk[-1]]
+	garbage_i, predict_i = split_by_year_week(pred_data, pred_yr_wk_t)
+	df_pred = pred_info.iloc[predict_i]
 
 	# set y_col
-	#y_cols = ['receiving_rec', 'receiving_tds', 'receiving_yds', 'rushing_att', 'rushing_tds','rushing_yds']
-	y_cols = ['rushing_yds']
+	y_cols = ['played', 'receiving_rec', 'receiving_tds', 'receiving_yds', 'rushing_att', 'rushing_tds','rushing_yds']
+	#y_cols = ['rushing_yds', 'played']
+
+	played_only = True
 
 	for y_col in y_cols:
 		# Pick the right columns
 		keep_like = [y_col] + lag_cols + mean_cols + other_cols
 		pickColumns = ExtractColumns(like=keep_like)
-		X_y = pickColumns.fit_transform(X=all_columns)
+		X_y = pickColumns.fit_transform(X=full_train)
+
+		if(played_only and y_col != 'played' and 'played' in X_y.columns):
+			played_bool = X_y['played'] == 1
+			X_y = X_y[played_bool]
+
 		# get X and y
 		y = X_y[y_col]
 		X = X_y.drop(y_col, axis=1)
+
 		# random split train and test
-		train_i, test_i = train_test_split_index(X.shape[0], test_size=0.05)
+		train_i, test_i = train_test_split_index(X.shape[0], test_size=0.1, seed=0)
 		# set up data
 		y_train = y.iloc[train_i]
 		y_test = y.iloc[test_i]
@@ -109,10 +170,10 @@ def main():
 		X_test = X.iloc[test_i]
 		### Test Predictions
 		# Gradident Boosting
-		gbr = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1).fit(X_train, y_train)
-		predict_test = gbr.predict(X_test)
-		gbr_rmse = mean_squared_error(y_test, predict_test)**0.5
-		gbr_mae = mean_absolute_error(y_test, predict_test)
+		gb = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1).fit(X_train, y_train)
+		predict_test = gb.predict(X_test)
+		gb_rmse = mean_squared_error(y_test, predict_test)**0.5
+		gb_mae = mean_absolute_error(y_test, predict_test)
 		# Random Forest Regressor
 		rf = RandomForestRegressor().fit(X_train, y_train)
 		predict_test = rf.predict(X_test)
@@ -124,14 +185,15 @@ def main():
 		lr_rmse = mean_squared_error(y_test, predict_test)**0.5
 		lr_mae = mean_absolute_error(y_test, predict_test)
 		# Print Results
-		print 'Predictng %s' % (y_col)
-		print 'Gradient Boosting: RMSE %.2f yards | MAE %.2f yards' % (gbr_rmse, gbr_mae)
-		print 'Random Forest: RMSE %.2f yards | MAE %.2f yards' % (rf_rmse, rf_mae)
-		print 'Linear Regression: RMSE %.2f yards | MAE %.2f yards' % (lr_rmse, lr_mae)
-		# Build full models on all but the last week & predict the last week
-		gbr = gbr.fit(X.iloc[train_i], y.iloc[train_i])
-		rf = rf.fit(X.iloc[train_i], y.iloc[train_i])
-		lr = lr.fit(X.iloc[train_i], y.iloc[train_i])
+		print 'Predicting %s' % (y_col)
+		print 'Gradient Boosting: RMSE %.2f | MAE %.2f' % (gb_rmse, gb_mae)
+		print 'Random Forest: RMSE %.2f | MAE %.2f' % (rf_rmse, rf_mae)
+		print 'Linear Regression: RMSE %.2f | MAE %.2f' % (lr_rmse, lr_mae)
+		# Build full models on all data
+
+		gb = gb.fit(X, y)
+		rf = rf.fit(X, y)
+		lr = lr.fit(X, y)
 		#### Next week's predictions
 		#pipe1 = pipe1.set_params(data__yr_wk=yr_wk_pred)
 		#data1 = pipe1.transform(X=None)
@@ -141,10 +203,11 @@ def main():
 		#X_pred = X_y_pred.drop(y_col, axis=1)
 		#y_pred = X_y_pred[y_col]
 		# Make prediction, just gbr for now
-		preds = gbr.predict(X.iloc[predict_i])
+		X_pred = pickColumns.fit_transform(X=pred_data).drop(y_col, axis=1)
+		preds = gb.predict(X_pred.iloc[predict_i])
 		df_pred[y_col] = preds
 
-	out_path = result_path + '/predictions' + '_' + str(int(yr_wk_pred[0][0])) + '_' + str(int(yr_wk_pred[0][1])) + '.json'
+	out_path = result_path + '/predictions' + '_' + str(int(pred_yr_wk_t[0][0])) + '_' + str(int(pred_yr_wk_t[0][1])) + '.json'
 	df_pred.to_json(path_or_buf = out_path, orient = 'records')
 
 if __name__ == "__main__":
