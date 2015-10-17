9c801e2970d22d89347d15e9ca7b9117f226cfe8
GIT Admin <younghak.jang@gmail.com>
2015-10-17 11:49:10 -0500

    added home/away teams

--- nfldb/get_RB.py
+++ nfldb/get_RB.py
@@ -12,15 +12,35 @@ stats = ['rushing_yds','rushing_att','rushing_tds','receiving_yds','receiving_ta
 player_info = ['player_id','full_name','position','team']
 
 player_list = []
+match_list = []
+
 for yr, wk in yr_wk:
 	q = nfldb.Query(db)
-	q.game(season_year=yr, week=wk, season_type='Regular').player(position='RB')	
+	q.game(season_year=yr, week=wk, season_type='Regular')
+
+	for games in q.as_games():
+		match = {'year':yr, 'week':wk}
+		match['home'] = games.home_team
+		match['away'] = games.away_team
+		match_list += [match]
+
+	q.player(position='RB')	
 	for pp in q.as_aggregate():
 		pobj = {'year':yr, 'week':wk}
 		for field in player_info:
 			pobj[field] = getattr(pp.player,field)
 		for field in stats:
 			pobj[field] = getattr(pp,field)
+		
+		for match in match_list:
+			if match['home'] == pobj['team']:
+				pobj['home'] = match['home']
+				pobj['away'] = match['away']
+				pobj['homeaway'] = 1
+			if match['away'] == pobj['team']:
+				pobj['home'] = match['home']
+				pobj['away'] = match['away']
+				pobj['homeaway'] = 0
 		player_list += [pobj]
 
 
@@ -46,80 +66,5 @@ pdf_conv = pdf
 for col in ['rushing_tds','receiving_tds']:
 	pdf_conv[col] = pdf[col] * tds_conv
 
-#grouped = pdf.groupby('player_id')
-
-pdf.to_csv('raw.csv')
-#grouped.to_csv('grouped.csv')
-
-
-#def make_lag_data_group(df, nlag=4, cols=['year', 'week', 'rushing_att', 'rushing_yds', 'rushing_loss_yds', 'rushing_tds'], same_year_bool=True):
-#	def lag_str(col, i):
-#		return col + '_lag' + str(i);
-#	dfout = df
-#	for i in range(1,nlag+1):
-#		dfi = df[cols].shift(i)
-#		dfi.columns = [lag_str(col, i) for col in dfi.columns]
-#		dfout = pd.concat([dfout, dfi], axis=1)
-#		if(same_year_bool):
-#			yr_lag = lag_str('year', i)
-#			dfout[lag_str('same_year',i)] = dfout['year'] == dfout[yr_lag]
-#			dfout.loc[pd.isnull(dfout[yr_lag]),lag_str('same_year',i)] = np.nan
-#	return dfout
-
-
-
-#def make_lag_data(df, nlag=4, groupby_cols = ['player_id'], cols=['year', 'week', 'rushing_att', 'rushing_yds']):
-#	grouped = df.groupby(groupby_cols)
-#	df_lag=None
-#	for name, group in grouped:
-#		dfi = make_lag_data_group(group, nlag=nlag)
-#		if(df_lag is None):
-#			df_lag = dfi
-#		else:
-#			df_lag = pd.concat([df_lag, dfi], axis=0)
-#	return(df_lag)
-
-#def pick_columns(df, like=['rushing_yds','rushing_yds_lag','rushing_att_lag','same_year_lag']):
-#	re_pat = re.compile('|'.join(like))
-#	col_names = df.columns.values.tolist()
-#	col_matches = [col for col in col_names if re_pat.search(col)]
-#	return df[col_matches]
-#
-#def drop_nan(df):
-#	return(df.dropna(axis=0))
-
-
-
-#pdf_lag = make_lag_data(pdf, nlag=4)
-#pdf_lag = pick_columns(pdf_lag)
-#pdf_lag = drop_nan(pdf_lag)
-
-
-#def train_test_split_index(n,test_size=0.2):
-#	rand_i = np.random.choice(range(n), n, replace=False)
-#	test_i = rand_i[range(int(round(n*test_size)))]
-#	train_i = rand_i[range(int(round(n*test_size)),n)]
-#	return train_i, test_i
-
-
-
-#y_col = 'rushing_yds'
-#y = pdf_lag[y_col]
-#X = pdf_lag.drop(y_col, axis=1)
-
-#train_i, test_i = train_test_split_index(X.shape[0], test_size=0.2)
-
-#y_train = y.iloc[train_i]
-#y_test = y.iloc[test_i]
-#X_train = X.iloc[train_i]
-#X_test = X.iloc[test_i]
-
-
-#from sklearn.cross_validation import train_test_split
-#from sklearn.ensemble import GradientBoostingRegressor
-
-
-#pdf_lag.head()
-
-
+pdf_conv.to_csv('raw.csv')
 
