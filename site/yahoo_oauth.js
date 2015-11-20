7d25f393eafa67e7287376203c11b357b6573bb4
Shelly Stanley <sjs1995@gmail.com>
2015-11-20 16:05:43 -0500

    add player info

--- site/yahoo_oauth.js
+++ site/yahoo_oauth.js
@@ -33,9 +33,9 @@ function showPlayers(obj, objName) {
       }
       
 	  var player = obj[i];
-	  result += "<h4>Player "+i+"</h4>";
+	  result += "<h4>Player "+(i+1)+"</h4>";
 	  console.log(player);
-	  result += "<div><a href="+player.image_url+">"+player.name.full+"</a> "+"player.eligible_positions"+"</div>";
+	  result += "<div><a href="+player.image_url+">"+player.name.full+"</a> "+player.eligible_positions.position+"</div>";
 
   }
   return result;
