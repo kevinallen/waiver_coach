6fd1213426dc6696dd65232840379e2798af4fa0
kevinallen <kgallen06@gmail.com>
2015-12-02 08:56:04 -0500

    working on hellojs moreteams api

--- site/hello/src/modules/yahoo.js
+++ site/hello/src/modules/yahoo.js
@@ -33,7 +33,8 @@
 				'players': yql('select * from fantasysports.players where league_key="348.l.1341932"'),
 				'teams': yql('select * from fantasysports.teams.roster where use_login=1 and game_key=348'),
 				'moreteams': function(p) {
-					var team = p.options.team;
+					console.log(p);
+					var team = p.team;
 					var queryStr = 'select * from fantasysports.teams.roster where team_key="' + team + '"';
 					yql(queryStr);
 				}
