4ac22145543e9ef28e1eafc3887630891292bc14
kevinallen <kgallen06@gmail.com>
2015-12-02 11:12:47 -0500

    working on hellojs moreteams api

--- site/yahoo_oauth.js
+++ site/yahoo_oauth.js
@@ -98,7 +98,7 @@ function getOtherPlayers(team_key) {
     		var teamID = myLeague + ".t." + i;
     	    var qdata = {team: teamID};
             var result = "";
-            var players_list = sessionStorage.getItem("t1");
+            var players_list = sessionStorage.getItem("t1").split(",");
     		hello( network ).api('moreteams', 'get', qdata).then(function(m){
               console.log(m);
               var team = m;
