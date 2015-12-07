93366afa1c015fb47c9fd819bdbefe9b132f0a91
kevinallen <kgallen06@gmail.com>
2015-12-06 20:19:15 -0600

    looking for bug

--- site/js/yahoo_oauth.js
+++ site/js/yahoo_oauth.js
@@ -1,53 +1,53 @@
 
 
-function showTeams(teams) {
-  var result = "";
-  document.getElementById("myteam").innerHTML = "";
-  var onlyOneTeam = false;
-  var players_list = [];
-  console.log(teams);
-  for (var i in teams) {
-      if (typeof teams[i] === null || typeof teams[i] !== "object") {
-          continue;
-      }
-      if (teams instanceof Array) {
-          var team = teams[i];
-	  } else {
-		  onlyOneTeam = true;
-          var team = teams;
-	  }
-	  if (team.roster.players === null) {
-		  continue;
-	  }
-      console.log(team);
-      // display team on page, and keep track of all players in players_list
-      var team_html = "<div class='col-sm-6'><h3 class=''>" + team.name + "</h3><p class='team'>";
-      var players = team.roster.players.player;
-      for (var j in players) {
-          var player = players[j];
-          players_list.push(team.roster.players.player[j].name.full);
-          var name = player.display_position == "DEF" ? player.name.first : player.name.first.substring(0,1) + ". " + player.name.last;
-          team_html += name + " - <span class='team-players'>" + player.display_position + "</span><br/>";
-      }
-      team_html += "</p></div>";
-      document.getElementById("myteam").innerHTML += team_html;
-      $('#myteam').show();
-
-      // put players_list in session storage and grab players from other teams
-	  if (typeof(Storage) !== "undefined") {
-		var league_team = {"myteam": players_list};
-		sessionStorage.setItem(team.name, JSON.stringify(league_team));
-		console.log(team.name, JSON.parse(sessionStorage.getItem(team.name)));
-	  } else {
-		alert("Your browser does not support web storage.  Please use a different browser to continue.");
-	  }
-
-	  if (onlyOneTeam) {
-		break;
-	  }
-  }
-  getOtherPlayers(team.team_key);
-}
+// function showTeams(teams) {
+//   var result = "";
+//   document.getElementById("myteam").innerHTML = "";
+//   var onlyOneTeam = false;
+//   var players_list = [];
+//   console.log(teams);
+//   for (var i in teams) {
+//       if (typeof teams[i] === null || typeof teams[i] !== "object") {
+//           continue;
+//       }
+//       if (teams instanceof Array) {
+//           var team = teams[i];
+// 	  } else {
+// 		  onlyOneTeam = true;
+//           var team = teams;
+// 	  }
+// 	  if (team.roster.players === null) {
+// 		  continue;
+// 	  }
+//       console.log(team);
+//       // display team on page, and keep track of all players in players_list
+//       var team_html = "<div class='col-sm-6'><h3 class=''>" + team.name + "</h3><p class='team'>";
+//       var players = team.roster.players.player;
+//       for (var j in players) {
+//           var player = players[j];
+//           players_list.push(team.roster.players.player[j].name.full);
+//           var name = player.display_position == "DEF" ? player.name.first : player.name.first.substring(0,1) + ". " + player.name.last;
+//           team_html += name + " - <span class='team-players'>" + player.display_position + "</span><br/>";
+//       }
+//       team_html += "</p></div>";
+//       document.getElementById("myteam").innerHTML += team_html;
+//       $('#myteam').show();
+//
+//       // put players_list in session storage and grab players from other teams
+// 	  if (typeof(Storage) !== "undefined") {
+// 		var league_team = {"myteam": players_list};
+// 		sessionStorage.setItem(team.name, JSON.stringify(league_team));
+// 		console.log(team.name, JSON.parse(sessionStorage.getItem(team.name)));
+// 	  } else {
+// 		alert("Your browser does not support web storage.  Please use a different browser to continue.");
+// 	  }
+//
+// 	  if (onlyOneTeam) {
+// 		break;
+// 	  }
+//   }
+//   getOtherPlayers(team.team_key);
+// }
 
 function parseLeagues(leagues) {
     var onlyOneLeague = false;
@@ -99,66 +99,66 @@ function parseLeagues(leagues) {
     }
 }
 
-function getOtherPlayers(team_key) {
-	var myLeague = team_key.split(".t")[0];  		// Get user's League number
-    var myTeam = Number(team_key.split("t.")[1]);   // Get user's team number
-	var network = 'yahoo';
-	var players_list = [];
-	hello( network ).api('league').then(function(d){
-	  var onlyOneLeague = false;
-      for (var j in d) {
-
-		  if (d instanceof Array) {
-			league = d[j];
-		  } else {
-			league = d;
-			onlyOneLeague = true;
-		  }
-
-          if (league.draft_status != "postdraft") {
-              continue;
-          }
-    	  for (var i = 1; i <= Number(league.num_teams); i++) {
-    		// if (i == myTeam) {
-    		//   continue;
-    		// }
-    		var teamID = myLeague + ".t." + i;
-    	    var qdata = {team: teamID};
-
-    		hello( network ).api('moreteams', 'get', qdata).then(function(m){
-              var team = m;
-              var players = team.roster.players.player;
-    		  for (var j in players) {
-                var player = players[j];
-                if (player.display_position = "RB") {
-                  players_list.push(player.name.full);
-                }
-              }
-              // Store other teams' players
-              if (typeof(Storage) !== "undefined") {
-                league_teams[team_name] = players_list;
-                sessionStorage.setItem(myLeague, JSON.stringify(players_list));
-                console.log(myLeague, JSON.parse(sessionStorage.getItem(myLeague)));
-              } else {
-                alert("Your browser does not support web storage.  Please use a different browser to continue.");
-              }
-            }).then(null, function(e){
-              console.error(e);
-            });
-    	  }
-
-		  if (onlyOneLeague) {
-			break;
-		  }
-	  }
-	}).then(null, function(e){
-		console.error(e);
-	});
-	setTimeout(function() {
-	  console.log("Other Teams' Players", JSON.parse(sessionStorage.getItem(myLeague)));
-	}, 2000);
-
-}
+// function getOtherPlayers(team_key) {
+// 	var myLeague = team_key.split(".t")[0];  		// Get user's League number
+//     var myTeam = Number(team_key.split("t.")[1]);   // Get user's team number
+// 	var network = 'yahoo';
+// 	var players_list = [];
+// 	hello( network ).api('league').then(function(d){
+// 	  var onlyOneLeague = false;
+//       for (var j in d) {
+//
+// 		  if (d instanceof Array) {
+// 			league = d[j];
+// 		  } else {
+// 			league = d;
+// 			onlyOneLeague = true;
+// 		  }
+//
+//           if (league.draft_status != "postdraft") {
+//               continue;
+//           }
+//     	  for (var i = 1; i <= Number(league.num_teams); i++) {
+//     		// if (i == myTeam) {
+//     		//   continue;
+//     		// }
+//     		var teamID = myLeague + ".t." + i;
+//     	    var qdata = {team: teamID};
+//
+//     		hello( network ).api('moreteams', 'get', qdata).then(function(m){
+//               var team = m;
+//               var players = team.roster.players.player;
+//     		  for (var j in players) {
+//                 var player = players[j];
+//                 if (player.display_position = "RB") {
+//                   players_list.push(player.name.full);
+//                 }
+//               }
+//               // Store other teams' players
+//               if (typeof(Storage) !== "undefined") {
+//                 league_teams[team_name] = players_list;
+//                 sessionStorage.setItem(myLeague, JSON.stringify(players_list));
+//                 console.log(myLeague, JSON.parse(sessionStorage.getItem(myLeague)));
+//               } else {
+//                 alert("Your browser does not support web storage.  Please use a different browser to continue.");
+//               }
+//             }).then(null, function(e){
+//               console.error(e);
+//             });
+//     	  }
+//
+// 		  if (onlyOneLeague) {
+// 			break;
+// 		  }
+// 	  }
+// 	}).then(null, function(e){
+// 		console.error(e);
+// 	});
+// 	setTimeout(function() {
+// 	  console.log("Other Teams' Players", JSON.parse(sessionStorage.getItem(myLeague)));
+// 	}, 2000);
+//
+// }
 
 function loggedIn(network) {
 
