38f6d3d8d618ec0cec79a20f849c75de6d78068b
kevinallen <kgallen06@gmail.com>
2015-12-08 07:13:39 -0600

    bug

--- site/js/player_table.js
+++ site/js/player_table.js
@@ -120,22 +120,22 @@ col_config = {
 json2table('../site_data/predictions.json', 'target_table', col_config)
 
 function filter_table() {
-	var running_backs = JSON.parse(sessionStorage.getItem("running_backs"));
-	console.log("players", players);
+	var taken_players = JSON.parse(sessionStorage.getItem("running_backs"));
+	console.log("taken_players", taken_players);
 
 	var selected_league = "";
 	$("#league_select option:selected").each(function(){
 		selected_league = $(this).val();
 	});
 	console.log("selected_league", selected_league);
-	league_players = players[selected_league];
+	league_players = taken_players[selected_league];
 	console.log("league_players", league_players);
 
 	var dynatable = $('#target_table').data('dynatable');
 
 	dynatable.queries.add("rush att", "13");
 	dynatable.queries.functions['rush att'] = function(record, queryValue) {
-		return running_backs.indexOf(record['dynatable-sortable-text'].name) == -1;
+		return league_players.indexOf(record['dynatable-sortable-text'].name) == -1;
 	};
 
 	dynatable.process();
